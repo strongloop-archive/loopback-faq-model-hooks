df7447b42901682f4a2affda1d54e036e2cd4e1b
Simon Ho <simon@strongloop.com>
2014-12-23 12:39:58 -0800

    Add update and destroy hooks

--- common/models/coffee-shop.js
+++ common/models/coffee-shop.js
@@ -1,3 +1,62 @@
 module.exports = function(CoffeeShop) {
+  // initialize
+  CoffeeShop.afterInitialize = function() {
+    console.log('> afterInitialize triggered');
+  };
 
+  // validate
+  CoffeeShop.beforeValidate = function(next, modelInstance) {
+    console.log('> beforeValidate triggered', modelInstance);
+    next();
+  };
+
+  CoffeeShop.afterValidate = function(next) {
+    console.log('> afterValidate triggered');
+    next();
+  };
+
+  // create
+  CoffeeShop.beforeCreate = function(next, modelInstance) {
+    console.log('> beforeCreate triggered', modelInstance);
+    next();
+  };
+
+  CoffeeShop.afterCreate = function(next) {
+    console.log('> afterCreate triggered');
+    next();
+  };
+
+  // save
+  CoffeeShop.beforeSave = function(next, modelInstance) {
+    console.log('> beforeSave triggered', modelInstance);
+    next();
+  };
+
+  CoffeeShop.afterSave = function(next) {
+    console.log('> afterSave triggered');
+    next();
+  };
+
+  // update
+  CoffeeShop.beforeUpdate = function(next, changes) {
+    console.log('> beforeUpdate triggered', changes);
+    console.log(arguments);
+    next();
+  };
+
+  CoffeeShop.afterUpdate = function(next) {
+    console.log('> afterUpdate triggered');
+    next();
+  };
+
+  // destroy
+  CoffeeShop.beforeDestroy = function(next) {
+    console.log('> beforeDestroy triggered');
+    next();
+  };
+
+  CoffeeShop.afterDestroy = function(next) {
+    console.log('> afterDestroy triggered');
+    next();
+  };
 };
---
+++ server/boot/sample-models.js
@@ -0,0 +1,30 @@
+var async = require('async');
+
+module.exports = function(app, cb) {
+  var CoffeeShop = app.models.CoffeeShop;
+
+  async.waterfall([
+    async.apply(createModel),
+    async.apply(updateModel),
+    async.apply(destroyModel)
+  ], cb);
+
+  function createModel(cb) {
+    console.log('\n--- Creating a model');
+
+    CoffeeShop.create({
+      name: 'Bel Cafe',
+      city: 'San Mateo'
+    }, cb);
+  }
+
+  function updateModel(coffeeShop, cb) {
+    console.log('\n--- Updating a model');
+    coffeeShop.updateAttribute('city', 'Vancouver', cb);
+  }
+
+  function destroyModel(coffeeShop, cb) {
+    console.log('\n--- Destroying a model');
+    coffeeShop.destroy(coffeeShop.id, cb);
+  }
+};
